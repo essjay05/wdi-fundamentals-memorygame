e7182c22cb192d8ecc0065cd519c6522654612f7
Joy Serquina <joyserquina@joys-mbp.home>
2018-10-23 21:05:45 -0700

    created game logic

--- MemoryGame/js/main.js
+++
@@ -1,7 +0,0 @@
-console.log("Up and running!");
-var cardOne = "queen"
-var cardTwo = "king"
-var cardThree = "king"
-var cardFour = "queen"
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardFour);
